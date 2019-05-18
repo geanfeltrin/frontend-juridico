1a83288e42618122fb263de54c79fe5ef04914c5
geanfeltrin <32302438+geanfeltrin@users.noreply.github.com>
2019-05-17 22:27:27 -0300

    create csv export

--- src/components/PdfList/index.js
+++ src/components/PdfList/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 
 import { Container } from "./styles";
-import { dropWhile } from "lodash";
+
 import { CSVLink, CSVDownload } from "react-csv";
 
 import Button from "@material-ui/core/Button";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Checkbox from "@material-ui/core/Checkbox";
+import { Table } from "@material-ui/core";
 
 export default class PdfList extends Component {
   state = {
@@ -28,12 +29,67 @@ export default class PdfList extends Component {
         }
       }
     }
-
-    console.log(
-      novo.map(novo => (novo.Tabela = [...novo.Tabela, " xSteps: 1"]))
-    );
+    console.log(novo);
 
     this.setState({ newData: novo });
+  };
+
+  exportCsv = () => {
+    const { newData } = this.state;
+    let keysHeader = [];
+    let valuesHeader = [];
+    let tableHeader = [];
+    let tableData = [];
+    let data = [];
+
+    for (let i = 0; i < 5; i++) {
+      const obj = Object.entries(newData[i]);
+      const tabela = Object.entries(newData[i].Tabela);
+      keysHeader = [];
+      valuesHeader = [];
+      tableHeader = [];
+      tableData = [];
+      if (tabela.length) {
+        for (let k = 0; k < tabela[0][1].length; k++) {
+          let aux = [];
+          for (t in tabela) {
+            aux.push(tabela[t][1][k]);
+          }
+          tableData.push(aux.join(";"));
+          tableData.push("%0A");
+        }
+      }
+      for (const [k, v] of obj) {
+        if (k != "Tabela") {
+          keysHeader.push(k);
+          valuesHeader.push(v);
+        }
+      }
+      for (const [k, v] of tabela) {
+        tableHeader.push(k);
+      }
+
+      let k = keysHeader.join(";");
+      let t = valuesHeader.join(";");
+      let z = tableHeader.join(";");
+      let v = tableData.join("");
+      console.log(typeof v);
+      data.push(k);
+      data.push(t);
+
+      data.push(z);
+      data.push(v);
+      data.push("%0A%0A%0A");
+    }
+
+    const csvString = data.join("%0A");
+
+    let a = document.createElement("a");
+    a.href = "data:attachment/csv," + csvString;
+    a.target = "_Blank";
+    a.download = "CNIS.csv";
+    document.body.appendChild(a);
+    a.click();
   };
 
   handleToggle = value => () => {
@@ -55,7 +111,6 @@ export default class PdfList extends Component {
 
   render() {
     const { pdfValue } = this.props;
-    // console.log(this.state.newData);
 
     return (
       <Container>
@@ -102,7 +157,8 @@ export default class PdfList extends Component {
         >
           PROCESSAR
         </Button>
-        <CSVLink data={this.state.newData}>Download me</CSVLink>;
+        {/* <CSVLink data={this.exportCsv}>Download me</CSVLink> */}
+        <Button onClick={this.exportCsv}>Download</Button>
       </Container>
     );
   }

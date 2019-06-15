9abddd64f380d509f6ca881adb9a9e7b0ac6a89b
geanfeltrin <32302438+geanfeltrin@users.noreply.github.com>
2019-06-14 21:55:46 -0300

    Updade calculo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from "react";
 import Main from "./pages/main/index";
 import GlobalStyle from "./styles/global";
+import Calculate from "./pages/Calculate";
 
 const App = () => (
   <Fragment>
     <GlobalStyle />
-    <Main />
+    <Calculate />
   </Fragment>
 );
 
---
+++ src/pages/Calculate/index.js
@@ -0,0 +1,173 @@
+import React, { Component } from "react";
+
+import { Container } from "./styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import api from "../../services/api";
+
+export default class Calculate extends Component {
+  state = {
+    data: [],
+    tabela: {
+      data: [
+        "05/14",
+        "07/14",
+        "08/14",
+        "01/1982",
+        "02/1982",
+        "03/1982",
+        "06/14",
+        "04/1982",
+        "05/1982",
+        "06/1982"
+      ],
+      aposentadoria: ["1000", "2000", "3000", "4000"],
+      aliquota: ["0.11", "0.11", "0.11", "0.11", "0.11", "0.11", "0.11", "0.11"]
+    },
+    newData: null
+  };
+
+  async componentDidMount() {
+    const response = await api.get("cnis/1");
+
+    console.log(response.data);
+    this.setState({ data: response.data });
+    this.getData();
+  }
+
+  getData() {
+    const { tabela, data } = this.state;
+    let newData = [];
+
+    if (data.data) {
+      for (let j = 0; j < data.data.length; j++) {
+        let loop = 0;
+        newData.push({
+          origem: "",
+          seq: "",
+          remuneracao: [],
+          aliquota: []
+        });
+        newData[j].origem = data.data[j].Origem_Do_Vinculo;
+        newData[j].seq = data.data[j].Seq;
+        for (let i = 0; i < data.data[j].Tabela.Competencia.length; i++) {
+          for (let k = loop; k < tabela.data.length; k++) {
+            newData[j].aliquota[k] = "0.11";
+            if (data.data[j].Tabela.Competencia[i] === tabela.data[k]) {
+              newData[j].remuneracao[k] = data.data[j].Tabela.Remuneracao[i]
+                .replace(/\./g, "")
+                .replace(/\,/g, ".");
+
+              loop = k + 1;
+              break;
+            } else {
+              newData[j].remuneracao[k] = 0;
+            }
+          }
+        }
+      }
+
+      this.setState({ newData: newData });
+    }
+  }
+
+  somatorio() {
+    const { newData } = this.state;
+  }
+
+  render() {
+    if (this.state.data.data) {
+      console.log(this.state.data.data.map(d => d));
+    }
+
+    return (
+      <Paper>
+        <Container>
+          <Table>
+            <TableHead />
+            <TableHead>
+              <TableRow>
+                <TableCell>Competência</TableCell>
+                <TableCell>Valor do Teto Previdenciário</TableCell>
+                <TableCell>Alíquota</TableCell>
+                <TableCell>
+                  Valor que Deveria ter sido Recolhido Alíquota 11%
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.tabela &&
+                this.state.tabela.data.map((d, i) => (
+                  <TableRow>
+                    <TableCell>{d}</TableCell>
+                    <TableCell>{this.state.tabela.aposentadoria[i]}</TableCell>
+                    <TableCell>{this.state.tabela.aliquota[i]}</TableCell>
+                    <TableCell>
+                      {this.state.tabela.aposentadoria[i] *
+                        this.state.tabela.aliquota[i]}
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+
+          {this.state.newData &&
+            this.state.newData.map((d, i) => (
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>
+                      {d.origem}: Extrato previdenciário. Seq.{d.seq}
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Salário de Contribuição</TableCell>
+                    <TableCell>Aliquota</TableCell>
+                    <TableCell>Valor Recolhido 11%</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {this.state.newData[i].remuneracao.map((r, j) => (
+                    <TableRow>
+                      <TableCell>{r}</TableCell>
+                      <TableCell>{this.state.newData[i].aliquota[j]}</TableCell>
+                      <TableCell>
+                        {(r * this.state.newData[i].aliquota[j]).toFixed(2)}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            ))}
+
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>VÍNCULO 01: Extrato previdenciário. Seq.</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableHead>
+              <TableRow>
+                <TableCell>Salário de Contribuição</TableCell>
+                <TableCell>Valor Recolhido 11%</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>1900</TableCell>
+                <TableCell>2000</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Container>
+      </Paper>
+    );
+  }
+}
